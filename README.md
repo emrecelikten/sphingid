fb976f62c422bc4f86c644aed71479bd51b31175
emrecelikten <emre+github@celikten.name>
2013-11-20 01:42:40 +0200

    Updating readme.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Sphingid
 =====
+
+Sphingid will be a high performance web spidering tool making extensive use of event-driven programming.
+
+Scala version: 2.10.3
+Dependencies: Akka, Spray, ScalaTest
--- src/test/scala/name/celikten/sphingid/SampleTest.scala
+++ src/test/scala/name/celikten/sphingid/SampleTest.scala
@@ -1,9 +1,18 @@
 package name.celikten.sphingid
 
+import org.scalatest.FlatSpec
+
 /**
  * User: emre (11/20/13 - 1:37 AM)
  */
 
-class SampleTest {
+class SampleTest extends FlatSpec {
+  "Sample test" should "work" in {
+    1 === 1
+  }
+
+  it should "still work" in {
+    1 === 1
+  }
 
 }
